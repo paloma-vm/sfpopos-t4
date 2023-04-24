5474dd25adf7fc8e345616f7ae3edf46adb538fb
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-04-24 00:21:33 -0600

    added Register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,6 +11,21 @@ function Register() {
 
         By signing up, you will be the first to know about events, news, and new art displays.
       </p>
+      
+      <form>
+        <fieldset>
+            <label htmlFor='nameInput' className='name'>Name<input className='nameInput' type="text" id="nameInput" placeholder="First and Last"/></label>
+            <label htmlFor='emailInput' className='email'>Email<input className='emailInput' type="email" id="emailInput" placeholder="Enter email"/></label>
+            <label htmlFor="checkbox" className='checkbox'><input type="checkbox" value="yes" id="checkbox"/> Yes, please send me the SFPOPOS Newsletter.</label>
+
+          <div className='buttons'>
+            <input type="submit" value="Cancel" id="Cancel"/>
+            <input type="submit" value="Submit" id="Submit"/>
+          </div>
+
+        </fieldset>
+      </form>
+
 
     </main>
   )
